17f1c08e6741f56bd15c6af5ecadc88b9fe6ccad
mosave <ae@aeinc.ru>
2022-07-17 20:40:37 +0300

    Работа над ошибками

--- custom-components/lvt/notify.py
+++ custom-components/lvt/notify.py
@@ -1,8 +1,7 @@
 """LVT support for notify component."""
-from socket import NI_NAMEREQD
 from typing import Any
 import voluptuous as vol
-from config.custom_components.lvt.lvt import LvtApi
+from .lvt import LvtApi
 from homeassistant.core import ServiceCall
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers import template
@@ -24,8 +23,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Optional(CONF_SPEAKER, default=""): cv.string,
         vol.Required(CONF_IMPORTANCE): cv.string,
-        vol.Optional(CONF_PLAY, default=""): cv.string,
-        vol.Optional(CONF_SAY, default=""): cv.string,
         vol.Optional(CONF_VOLUME, default=""): cv.string,
     }
 )
@@ -50,26 +47,12 @@ class LVTNotificationService(BaseNotificationService):
                     self.volume = 0
                 elif self.volume > 100:
                     self.volume = 100
-            except:
+            except ValueError:
                 self.volume = None
-
-        say_template = config[CONF_SAY]
-        if bool(config[CONF_PLAY]):
-            self.play = template.Template(config[CONF_PLAY], hass)
-        else:
-            self.play = None
-            if not bool(say_template):
-                say_template = " {% if title is defined %}{{ title }}{% endif %} .;,- {{ message }}"
-
-        if bool(say_template):
-            self.say = template.Template(say_template, hass)
-        else:
-            self.say = None
 
     async def async_send_message(self, message="", **kwargs):
         """Send a message to LVT."""
         lvt_api: LvtApi = self.hass.data[DOMAIN]
-        kwargs["message"] = message
         data = kwargs.get(ATTR_DATA)
         if data is None:
             data = {}
@@ -77,14 +60,8 @@ class LVTNotificationService(BaseNotificationService):
         speaker = kwargs.get("trigger")
         data["speaker"] = speaker if bool(speaker) else self.speaker
         data["importance"] = self.importance
-        data["volume"] = self.volume
-
-        if self.play is not None:
-            data["play"] = self.play.async_render(kwargs)
-            call = ServiceCall(DOMAIN, "lvt.play", data=data)
-            await lvt_api.handle_play(call)
-
-        if self.say is not None:
-            data["say"] = self.say.async_render(kwargs)
-            call = ServiceCall(DOMAIN, "lvt.say", data=data)
-            await lvt_api.handle_say(call)
+        volume = kwargs.get("volume")
+        data["volume"] = volume if volume is not None else self.volume
+        data["say"] = message if bool(message) else "Текст сообщения не задан"
+        call = ServiceCall(DOMAIN, "lvt.say", data=data)
+        await lvt_api.handle_say(call)
